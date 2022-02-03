5460ef6e54639b4041452835d74dd2b200a43c34
Pia Chouaifaty <piachouaifaty@Pias-MacBook-Pro.local>
2022-02-03 15:00:13 +0200

    initializing variables, begin setup of onclick functions

--- currency_converter_lbp_usd/app/src/main/java/com/example/currency_converter_lbp_usd/MainActivity.java
+++ currency_converter_lbp_usd/app/src/main/java/com/example/currency_converter_lbp_usd/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.currency_converter_lbp_usd;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initializing variable and linking it to layout object
+        Button to_usd_button = (Button) findViewById(R.id.LBPtoUSDButton);
+        int exchange_rate = 22000;
+
+        //onclick event
+        to_usd_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText lbp_amount = (EditText) findViewById(R.id.LBPAmountEditText);
+
+            }
+        });
+
+        //initializing variable and linking it to layout object
+        Button to_lbp_button = (Button) findViewById(R.id.USDtoLBPButton);
+
+        to_lbp_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText usd_amount = (EditText) findViewById(R.id.USDAmountEditText);
+
+            }
+        });
+
+
     }
 }
